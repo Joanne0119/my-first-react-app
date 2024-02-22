7721edf4c98dd38fb161733cfb8032aad11207c2
Joanne0119 <liuchengen52033@gmail.com>
2024-02-23 00:32:24 +0800

    using useState to loop the movies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import './App.css';
 import SearchIcon from './search.svg';
@@ -8,25 +8,27 @@ import MovieCard from './MovieCard'
 
 const API_URL = 'http://www.omdbapi.com?apikey=903351d2'
 
-const movie1 = {
-  "Title": "Batman v Superman: Dawn of Justice",
-  "Year": "2016",
-  "imdbID": "tt2975590",
-  "Type": "movie",
-  "Poster": "https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
-}
+// const movie1 = {
+//   "Title": "Batman v Superman: Dawn of Justice",
+//   "Year": "2016",
+//   "imdbID": "tt2975590",
+//   "Type": "movie",
+//   "Poster": "https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+// }
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
 
   const searchMovies = async(title) => {
     const response = await fetch(`${API_URL}&s=${title}`) //to pull api
     const data = await response.json();
 
     console.log(data.Search);
+    setMovies(data.Search);
   }
 
   useEffect(() => {
-    searchMovies('Superman');
+    searchMovies('Batman');
   },[])
 
   return (
@@ -45,9 +47,21 @@ const App = () => {
           onClick={() => {}}
         />
       </div>
-      <div className='container'>
-        <MovieCard movie1 = {movie1} />
-      </div>
+
+      {
+        movies?.length > 0
+          ? (
+            <div className='container'>
+              {movies.map((movie) => (
+                <MovieCard movie = {movie} />
+              ))}
+            </div>
+          ) : (
+            <div className='empty'>
+              <h2>No movies found</h2>
+            </div>
+          )
+      }
     </div>
   );
 }
