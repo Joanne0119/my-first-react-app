9517b9b09810d346712a0695bc1357877dbfe40a
Joanne0119 <liuchengen52033@gmail.com>
2024-02-22 23:46:10 +0800

    testing api with useEffect()

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ import { useEffect } from 'react';
 const API_URL = 'http://www.omdbapi.com?apikey=903351d2'
 
 const App = () => {
+
+  const searchMovies = async(title) => {
+    const response = await fetch(`${API_URL}&s=${title}`) //to pull api
+    const data = await response.json();
+
+    console.log(data.Search);
+  }
+
+  useEffect(() => {
+    searchMovies('Superman');
+  },[])
+
   return (
     <h1>App</h1>
   );
